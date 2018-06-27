bb875c13c84b2613c77ccb2101e0fe72ac1867ce
lg <lg4869@outlook.com>
2018-06-27 22:14:22 +0800

    move tools to a package

--- main/main.go
+++ main/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"time"
-)
 
-const (
-	endpoint = `http://cd.lianjia.com`
+	"github.com/echoturing/buyhouse/linkhome"
 )
 
 var (
@@ -17,31 +14,8 @@ var (
 func main() {
 	timestamp := time.Now().Unix()
 	for _, blockName := range blockNames {
-		items := run(blockName, 1)
+		items := linkhome.CrowlBlock(blockName, 1)
 		resultFile := fmt.Sprintf("result_%s_%d.txt", blockName, timestamp)
-		WriteHouseInfoToFile(resultFile, items)
+		linkhome.SaveHouseInfo(resultFile, items)
 	}
-}
-
-func run(blockName string, maxPage int) []HouseInfo {
-	var allItems []HouseInfo
-	var prevItems []HouseInfo
-	for i := 1; i <= maxPage; i++ {
-		requestURL := fmt.Sprintf("%s/ershoufang/%s/pg%d/", endpoint, blockName, i)
-		log.Printf("crawling page %d from %s", i, requestURL)
-		res, err := GetItemFromURL(requestURL)
-		if err != nil {
-			log.Printf("failed to crawl %s, err: %v", requestURL, err)
-			continue
-		}
-		if NeedContinue(prevItems, res) {
-			prevItems = res
-			allItems = append(allItems, res...)
-		} else {
-			fmt.Println("发现重复的,停止")
-			break
-		}
-		time.Sleep(time.Second * 5)
-	}
-	return allItems
 }
